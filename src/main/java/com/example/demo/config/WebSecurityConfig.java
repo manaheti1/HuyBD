3f6c9791a023fb7af992fd81f820b0146fdea248
ntnghia4 <nguyentuannghia47@gmail.com>
2022-07-22 16:39:07 +0700

    test push by Eclipse

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.example.demo.config;
 
-public class WebSecurityConfig {
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 
 }
